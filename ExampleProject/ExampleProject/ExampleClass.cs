7ddd805a1fe6108607b7a87dc7fffbdcab041dca
bobroshaa <bobroshaa@mail.ru>
2022-12-14 09:50:39 +0300

    add example

---
+++ ExampleProject/ExampleProject/ExampleClass.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+namespace ExampleProject
+{
+    namespace FirstExample
+    {
+
+        public class ExampleClass
+        {
+            private int _param1;
+            private string[] _param2;
+
+            public int Param1
+            {
+                get => _param1;
+                set => _param1 = value;
+            }
+
+            public string[] Param2
+            {
+                get => _param2;
+                set => _param2 = value;
+            }
+
+            public int Sum(int num1, int num2)
+            {
+                return num1 + num2;
+            }
+
+            public int Des(int num1, int num2)
+            {
+                return num1 - num2;
+            }
+
+            public int Mul(int num1, int num2)
+            {
+                return num1 * num2;
+            }
+
+            private void HiddenMethod()
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ ExampleProject/ExampleProject/ExampleClass2.cs
@@ -0,0 +1,30 @@
+﻿namespace ExampleProject;
+
+public class ExampleClass2
+{
+    private IKitten _num;
+    private IPuppy _bebra;
+    private int _chislo;
+
+    public ExampleClass2(IKitten num, IPuppy bebra, int chislo)
+    {
+        _num = num;
+        _bebra = bebra;
+        _chislo = chislo;
+    }
+    
+    public int Method1(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public void Method2(int n)
+    {
+        return;
+    }
+
+    private void HiddenMethod()
+    {
+        return;
+    }
+}
